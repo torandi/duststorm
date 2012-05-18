8728ba644c55b272120b8367c5c649c84bda6cf7
David Sveningsson <ext@sidvind.com>
2012-05-18 14:42:58 +0200

    Stub functions.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,42 @@
 #include "config.h"
 #endif
 
+#include <cstdio>
+#include <cstdlib>
+#include <signal.h>
+
+static volatile bool running = true;
+
+void handle_sigint(int signum){
+	if ( !running ){
+		fprintf(stderr, "\rgot SIGINT again, aborting\n");
+		abort();
+	}
+
+	running = false;
+	fprintf(stderr, "\rgot SIGINT, terminating graceful\n");
+}
+
+static void init(){
+
+}
+
+static void cleanup(){
+
+}
+
+static void magic_stuff(){
+	while ( running ){
+
+	}
+}
+
 int main(int argc, char* argv[]){
+	signal(SIGINT, handle_sigint);
+
+	init();
+	magic_stuff();
+	cleanup();
+
 	return 0;
 }
