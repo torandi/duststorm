cf04dfdc121ee180e3f3a64954516ffcee50d179
Andreas Tarandi <torandi@gmail.com>
2012-05-18 19:50:58 +0200

    Add uniform shader include

---
+++ shaders/uniforms.glsl
@@ -0,0 +1,16 @@
+uniform mat4 projectionViewMatrix;
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+uniform mat4 normalMatrix;
+
+uniform vec3 camera_pos;
+
+//Light data
+uniform float light_attenuation;
+uniform vec4 light_intensity;
+uniform vec4 light_position;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
