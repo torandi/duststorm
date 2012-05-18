7ea8a04eaed45f88f90672fb6022d366f3c33d55
David Sveningsson <ext@sidvind.com>
2012-05-18 14:54:24 +0200

    window.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <signal.h>
+#include <SDL/SDL.h>
+#include <GL/glew.h>
 
 static volatile bool running = true;
 
@@ -19,16 +21,55 @@ void handle_sigint(int signum){
 }
 
 static void init(){
+	if ( SDL_Init(SDL_INIT_VIDEO) != 0 ){
+		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+		exit(1);
+	}
 
+	SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
+	SDL_SetVideoMode(800, 600, 0, SDL_OPENGL|SDL_DOUBLEBUF);
+	SDL_EnableKeyRepeat(0, 0);
+
+	int ret;
+	if ( (ret=glewInit()) != GLEW_OK ){
+		fprintf(stderr, "Failed to initialize GLEW: %s\n", glewGetErrorString(ret));
+		exit(1);
+	}
+
+	glClearColor(1,0,1,1);
 }
 
 static void cleanup(){
 
 }
 
+static void poll(){
+	SDL_Event event;
+	while ( SDL_PollEvent(&event) ){
+		switch ( event.type ){
+		case SDL_QUIT:
+			running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if ( event.key.keysym.sym == SDLK_ESCAPE ){
+				running = false;
+			}
+			if ( event.key.keysym.sym == SDLK_q && event.key.keysym.mod & KMOD_CTRL ){
+				running = false;
+			}
+		}
+	}
+}
+
+static void render(){
+
+}
+
 static void magic_stuff(){
 	while ( running ){
-
+		poll();
+		render();
 	}
 }
 
