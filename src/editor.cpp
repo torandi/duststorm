4f526375c8ba0c64cd30f12aa1a7bf5ad8e7f9bb
David Sveningsson <ext@sidvind.com>
2012-05-27 21:13:57 +0200

    Move time by panning on label.

--- src/editor.cpp
+++ src/editor.cpp
@@ -29,6 +29,7 @@ static GtkLabel* timelabel = nullptr;
 static GtkToggleButton* playbutton = nullptr;
 static RenderTarget* frame = nullptr;
 static Scene* scene = nullptr;
+static float slide_ref;
 
 static void show_fps(int signum){
 	fprintf(stderr, "FPS: %d\n", frames);
@@ -204,6 +205,28 @@ extern "C" G_MODULE_EXPORT void play_toggled_cb(GtkWidget* widget, gpointer data
 		gtk_button_set_image(GTK_BUTTON(widget), gtk_image_new_from_stock("gtk-media-play", GTK_ICON_SIZE_SMALL_TOOLBAR));
 		global_time.set_paused(true);
 	}
+}
+
+extern "C" G_MODULE_EXPORT void timelabel_button_press_event_cb(GtkWidget* widget, GdkEvent* event, gpointer data){
+	global_time.set_paused(true);
+	gtk_toggle_button_set_active(playbutton, FALSE);
+
+	slide_ref = event->button.x;
+	gdk_device_grab(gdk_event_get_device(event), gtk_widget_get_parent_window(widget),
+	                GDK_OWNERSHIP_WINDOW, TRUE, GDK_BUTTON_RELEASE_MASK, NULL, event->button.time);
+}
+
+extern "C" G_MODULE_EXPORT void timelabel_button_release_event_cb(GtkWidget* widget, GdkEvent* event, gpointer data){
+	gdk_device_ungrab(gdk_event_get_device(event), event->button.time);
+}
+
+extern "C" G_MODULE_EXPORT void timelabel_motion_notify_event_cb(GtkWidget* widget, GdkEvent* event, gpointer data){
+	const float dx = slide_ref - event->motion.x;
+	const float adj = dx * 0.6; /* >0.5 so 1px will always count to something */
+	const int steps = (int)roundf(adj);
+
+	global_time.step(steps);
+	slide_ref = event->motion.x;
 }
 
 void update(){
