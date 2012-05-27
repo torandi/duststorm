972a33d0337a8fe23f5fe05b414f468de9d2fd26
Andreas Tarandi <torandi@gmail.com>
2012-05-28 01:29:01 +0200

    Begin config of particle system

--- src/scene/particles.cpp
+++ src/scene/particles.cpp
@@ -15,7 +15,13 @@ public:
 		, camera(75.f, size.x/(float)size.y, 0.1f, 100.f) {
 			camera.set_position(glm::vec3(0.f, 0.f, -1));
 			camera.look_at(glm::vec3(0.f, 0.f, 0.f));
-			particles.config.spawn_position = glm::vec4(-1.f, -0.5f, 0.f, 1.f);
+			particles.config.spawn_position = glm::vec4(-1.f, -0.5f, -1.f, 1.f);
+			particles.config.spawn_area = glm::vec4(2.f, 0.f, 2.f, 0.f);
+			particles.config.spawn_direction = glm::vec4(0, 1.f, 0.f, 0.f);
+			particles.config.avg_spawn_speed= 0.001f;
+			particles.config.spawn_speed_var = 0.0005f;
+			particles.config.avg_scale = 0.001f;
+			particles.config.scale_var = 0.0005f;
 			particles.update_config();
 	}
 
