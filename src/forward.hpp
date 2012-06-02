4c04a3b00708c25047eeeba06d50aaaa4cdbb7d7
David Sveningsson <ext@sidvind.com>
2012-06-02 14:12:34 +0200

    Adding forward declarations.

---
+++ src/forward.hpp
@@ -0,0 +1,16 @@
+class Camera;
+class CL;
+class Color;
+class Light;
+class Mesh;
+class MovableObject;
+class ParticleSystem;
+class PointTable;
+class Quad;
+class RenderObject;
+class RenderTarget;
+class Scene;
+class Shader;
+class Texture;
+class TimeTable;
+class XYLerpTable;
