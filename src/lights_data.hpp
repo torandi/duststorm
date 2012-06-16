d6afe36d5bebf6ed498597c28120340ddc12953b
David Sveningsson <ext@sidvind.com>
2012-06-16 14:48:11 +0200

    cosmetic: reindent and realign.

--- src/lights_data.hpp
+++ src/lights_data.hpp
@@ -1,24 +1,24 @@
 #ifndef LIGHTS_DATA_H
 #define LIGHTS_DATA_H
 
+#include "movable_light.hpp"
+#include "shader.hpp"
 #include <glm/glm.hpp>
 
-#include "shader.hpp"
-#include "movable_light.hpp"
-
 class LightsData {
-	public:
-		LightsData();
-		~LightsData();
+public:
+	LightsData();
+	~LightsData();
 
-		MovableLight *lights[MAX_NUM_LIGHTS];
-		
-		glm::vec3 &ambient_intensity() { return data_.ambient_intensity; };
-		int &num_lights() { return data_.num_lights; };
+	MovableLight *lights[MAX_NUM_LIGHTS];
 
-		const Shader::lights_data_t &shader_data();
-	private:
-		Shader::lights_data_t data_;
+	glm::vec3 &ambient_intensity() { return data_.ambient_intensity; };
+	int &num_lights() { return data_.num_lights; };
+
+	const Shader::lights_data_t &shader_data();
+
+private:
+	Shader::lights_data_t data_;
 };
 
 #endif
