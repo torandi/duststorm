73d10f3b9b2515ace9d1ee180e7503abce792371
David Sveningsson <ext@sidvind.com>
2012-06-16 13:54:52 +0200

    Using compiler alignment instead of manual padding.
    It makes initializing the struct easier as you can use "material_t foo = {..}".

--- src/shader.hpp
+++ src/shader.hpp
@@ -56,8 +56,7 @@ public:
 
 	struct material_t {
 		float shininess;
-		float padding[3];
-		glm::vec4 diffuse;
+		glm::vec4 diffuse __attribute__ ((aligned (16)));
 		glm::vec4 specular;
 		glm::vec4 ambient;
 		glm::vec4 emission;
