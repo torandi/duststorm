dd4670326d992a5dfe97ef8bddfb5e04ae54c7a7
David Sveningsson <ext@sidvind.com>
2012-06-16 17:09:36 +0200

    Attenuation is applied last, specular does not depend on accumulated diffuse.

--- shaders/light_calculations.glsl
+++ shaders/light_calculations.glsl
@@ -6,36 +6,31 @@ vec4 computeLighting(
 	in light_data light, in vec4 originalColor,
 	in vec3 N, in vec3 L,
 	in vec3 camera_dir, in float distance,
-	float shininess, vec4 specular,
+	float shininess, vec4 specular_color,
 	bool use_diffuse, bool use_specular
 	) {
-	vec3 lightIntensity;
-
-	//Light attenuation on if type == 1 (point light)
-	if(light.type == 0) {
-		lightIntensity = light.intensity.rgb;
-	} else {
-		float lightAttenuation = attenuation(light, distance);
-		lightIntensity =  lightAttenuation * light.intensity.rgb;
-	}
 
 	float LambertTerm = max( dot(L, N), 0.0);
-	float specular_amount = 0.0;
 
+	float specular_amount = 0.0;
 	if( LambertTerm > 0.0) {
-		//Apply specular
 		specular_amount = pow(clamp(dot(reflect(-L, N), camera_dir), 0.0, 1.0), shininess);
 	}
 
-	vec3 diffuse = originalColor.rgb * LambertTerm * lightIntensity;
-	vec3 specular_color = specular.rgb * specular_amount * length(diffuse);
+	vec3 diffuse  = light.intensity.rgb * originalColor.rgb * LambertTerm;
+	vec3 specular = specular_color.rgb * specular_amount;
 
 	vec4 color = vec4(0.0);
 	if(use_diffuse)
 		color.rgb += diffuse;
 	if(use_specular)
-		color.rgb += specular_color;
+		color.rgb += specular;
 	color.a = 1.0;
 
-	return color;
+	float attn = 1.0;
+	if ( light.type == 1 ){ /* point light */
+		attn = attenuation(light, distance);
+	}
+
+	return color * attn;
 }
