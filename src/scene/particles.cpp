c63d96f3817ef6d108b407233668f988968290c4
Andreas Tarandi <torandi@gmail.com>
2012-06-18 00:00:00 +0200

    Playing around with explosions

--- src/scene/particles.cpp
+++ src/scene/particles.cpp
@@ -11,12 +11,11 @@ class ParticlesScene: public Scene {
 public:
 	ParticlesScene(const glm::ivec2 &size)
 		: Scene(size)
-		, fire(1000,  TextureArray::from_filename("fire1.png", "fire2.png", "fire3.png", nullptr))
-		, smoke(1000, TextureArray::from_filename("fog.png", nullptr))
+		, expl(500000, TextureArray::from_filename("particle.png", nullptr))
 		, camera(75.f, size.x/(float)size.y, 0.1f, 100.f) {
 			camera.set_position(glm::vec3(0.f, 0.f, -1));
 			camera.look_at(glm::vec3(0.f, 0.f, 0.f));
-			fire.config.spawn_position = glm::vec4(0.f, -0.5f, -0.2f, 1.f);
+			/*fire.config.spawn_position = glm::vec4(0.f, -0.5f, -0.2f, 1.f);
 			fire.config.spawn_area = glm::vec4(0.0f, 0.f, 0.0f, 0.2f);
 			fire.config.spawn_direction = glm::vec4(0, 1.f, 0.f, 0.f);
 			fire.config.direction_var = glm::vec4(0.1f, 0.f, 0.1f, 0.f);
@@ -54,14 +53,35 @@ public:
 			smoke.config.birth_color = glm::vec4(0.2, 0.2, 0.2, 0.5);
 			smoke.config.death_color = glm::vec4(0.8 ,0.8, 0.8, 0.f);
 			smoke.config.motion_rand = glm::vec4(0.001f, 0.f, 0.001f, 0);
-			smoke.update_config();
+			smoke.update_config();*/
+
+			expl.avg_spawn_rate = 1000000.f;
+			expl.spawn_rate_var = 0.f;
+			expl.config.spawn_position = glm::vec4(0.f, 0.f, 2.f, 1.f);
+			expl.config.spawn_area = glm::vec4(0.0f, 0.f, 0.0f, 0.0f);
+			expl.config.spawn_direction = glm::vec4(0.f, 0.f, 0.f, 0.f);
+			expl.config.direction_var = glm::vec4(1.1f, 1.f, 1.1f, 0.f);
+			expl.config.avg_spawn_speed= 0.01f;
+			expl.config.spawn_speed_var = 0.005f;
+			expl.config.avg_ttl = 5.f;
+			expl.config.ttl_var = 2.5f;
+			expl.config.avg_scale = 0.01f;
+			expl.config.scale_var = 0.005f;
+			expl.config.avg_scale_change = 0.001f;
+			expl.config.scale_change_var = 0.0005f;
+			expl.config.avg_rotation_speed = 0.f;
+			expl.config.rotation_speed_var = 0.f;
+			expl.config.birth_color = glm::vec4(1.0, 0.0, 0.0, 1.0);
+			expl.config.death_color = glm::vec4(0.0 ,1.0, 0.0, 0.f);
+			expl.config.motion_rand = glm::vec4(0.0, 0.f, 0.f, 0);
+			expl.update_config();
 	}
 
 	virtual void render_geometry(const Camera& cam){
 		Shader::upload_camera(camera);
 		shaders[SHADER_PARTICLES]->bind();
-		smoke.render();
-		fire.render();
+		//smoke.render();
+		expl.render();
 	}
 
 	virtual void render(){
@@ -75,8 +95,9 @@ public:
 	}
 
 	virtual void update(float t, float dt){
-		fire.update(dt);
-		smoke.update(dt);
+		expl.update(dt);
+		expl.avg_spawn_rate = 0.f;
+		//smoke.update(dt);
 
 		#ifdef ENABLE_INPUT
 			input.update_object(camera, dt);
@@ -87,7 +108,7 @@ public:
 	}
 
 private:
-	ParticleSystem fire, smoke;
+	ParticleSystem expl;
 	Camera camera;
 };
 
