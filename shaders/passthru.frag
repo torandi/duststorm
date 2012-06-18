d32f0dbccacbdcd49642dd6e87a6a3a2b43c9458
David Sveningsson <ext@sidvind.com>
2012-06-18 13:56:21 +0200

    Take sampler as argument.

--- shaders/passthru.frag
+++ shaders/passthru.frag
@@ -5,6 +5,6 @@
 out vec4 ocolor;
 
 void main(){
-	vec2 uv = screenspace_uv();
+	vec2 uv = screenspace_uv(texture0);
 	ocolor = texture2D(texture0, uv);
 }
--- shaders/screenspace.glsl
+++ shaders/screenspace.glsl
@@ -1,8 +1,7 @@
 /**
- * Calculate screenspace UV based on texture unit 0.
- * This is intended for shaders dealing with post-effects only.
+ * Calculate screenspace UV based on texture.
  */
-vec2 screenspace_uv(){
-	ivec2 size = textureSize(texture0, 0);
+vec2 screenspace_uv(sampler2D t){
+	ivec2 size = textureSize(t, 0);
 	return vec2(gl_FragCoord.x / size.x, gl_FragCoord.y / size.y);
 }
