fcda5f9c4d3b3381c5f8821090bc113211b801d0
Andreas Tarandi <torandi@gmail.com>
2012-07-11 00:13:46 +0200

    move light with callback

--- src/player.cpp
+++ src/player.cpp
@@ -3,4 +3,5 @@
 
 Player::Player(const YAML::Node &node) {
 	light.intensity = node["light"].as<glm::vec3>(glm::vec3(0.8f));
+	add_position_callback(&light, glm::vec3(0.f, node["light_height"].as<float>(0.1f), 0.f));
 }
