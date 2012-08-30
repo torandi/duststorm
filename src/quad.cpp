cc5fccb8008ea6cebc4f18eb59b8e1ce4ec8328f
Andreas Tarandi <torandi@gmail.com>
2012-08-30 23:44:16 +0200

    Less magic numbers in Quad

--- src/quad.cpp
+++ src/quad.cpp
@@ -3,24 +3,27 @@
 
 #include <cstring>
 
-const float Quad::vertices[][5] = { /* x,y,z,u,v */
+static const int NUM_VERTICES = 4;
+static const int NUM_VERTEX_DATA_POINTS = 5;
+
+const float Quad::vertices[][NUM_VERTEX_DATA_POINTS] = { /* x,y,z,u,v */
 	{1.f, 1.f, 0.f, 1.f, 1.f},
 	{1.f, 0.f, 0.f, 1.f, 0.f},
 	{0.f, 0.f, 0.f, 0.f, 0.f},
 	{0.f, 1.f, 0.f, 0.f, 1.f},
 };
 
-const unsigned int Quad::indices[6] = {0,1,2,3,0,2};
+const unsigned int Quad::indices[] = {0,1,2,3,0,2};
 
 Quad::Quad(glm::vec2 texture_scale, bool normal, bool tangent_and_bitangent) : Mesh() {
-	float v[4][5];
+	float v[NUM_VERTEX_DATA_POINTS][NUM_VERTEX_DATA_POINTS];
 	memcpy(v, vertices, 20*sizeof(float));
-	for(int i=0; i <5; ++i) {
-		v[i][3]*=texture_scale.x;
-		v[i][4]*=texture_scale.y;
+	for(int i=0; i <NUM_VERTICES; ++i) {
+		v[i][3]*=texture_scale.x; //u
+		v[i][4]*=texture_scale.y; //v
 	}
-	set_vertices(v, 4);
-	set_indices(std::vector<unsigned int>(indices, indices+6));
+	set_vertices(v, NUM_VERTICES);
+	set_indices(std::vector<unsigned int>(indices, indices+sizeof(indices)));
 
 	if(normal) generate_normals();
 	if(tangent_and_bitangent) generate_tangents_and_bitangents();
